8d1ac1c9a2ad76855205f391d6e6cc64ce9ca093
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2022-06-24 18:26:45 -0300

    toqueteo variado

--- src/main/java/modelo/tickets/locaciones/ILocacion.java
+++ src/main/java/modelo/tickets/locaciones/ILocacion.java
@@ -1,8 +1,12 @@
 package modelo.tickets.locaciones;
 
 public interface ILocacion {
+
     int puntajeLocacion( ILocacion locacion);
+
     int puntajeHomeOffice();
+
     int puntajeIndistinto();
+
     int puntajePresencial();
 }

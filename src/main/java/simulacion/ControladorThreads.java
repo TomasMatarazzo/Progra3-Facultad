0ef842476e2963b2fc623d7b428b3c471032d06f
Mati Angelico <mnangelico@gmail.com>
2022-06-25 22:50:44 -0300

    stop simulacion

--- src/main/java/simulacion/ControladorThreads.java
+++ src/main/java/simulacion/ControladorThreads.java
@@ -4,41 +4,57 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.ArrayList;
+
 import modelo.Sistema;
 import vista.IVista;
 import javax.swing.*;
 
 public class ControladorThreads implements ActionListener, WindowListener {
+	
 	private IVista vista;
-
+	private ArrayList<Thread> threads = new ArrayList<>();
+	
 	public ControladorThreads(IVista vista) {
 		this.vista = vista;
 		this.vista.setActionListener(this);
 	}
+
+	public ArrayList<Thread> getThreads() {
+		return threads;
+	}
+
+	public void agregarThread(Thread t){
+        this.threads.add(t);
+    }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Thread q;
 		String comando = e.getActionCommand();
 		if (comando.equalsIgnoreCase("Comenzar")) {
-			for (int t = 0;t < Sistema.getInstance().getEmpleadores().size();t++) {
+			for (int t = 0;t < 5;t++) {
 				q = new Thread(Sistema.getInstance().getEmpleadores().get(t));
+				this.agregarThread(q);
 				q.start();
 			}
-			for (int t = 0;t < Sistema.getInstance().getEmpleadosPretensos().size();t++) {
+			for (int t = 0;t < 5;t++) {
 				q = new Thread(Sistema.getInstance().getEmpleadosPretensos().get(t));
+				this.agregarThread(q);
 				q.start();
 			}
 		} else
 			if (comando.equalsIgnoreCase("Volver")) {
-				//ACA DEBERIA DETENER TODOS LOS HILOS
+				for (int t = 0;t < this.threads.size();t++) { 
+					this.threads.get(t).stop();
+				}
 				vista.creaOtraVentana("Login");
 				vista.cerrarVentana();
 				JOptionPane.showMessageDialog(null, "Se ha detenido la simulacion");
 				
 			}
 	}
-
+	
 	@Override
 	public void windowClosing(WindowEvent e) {
 		//PONGO ESTO?
